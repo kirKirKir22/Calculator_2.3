cb6e66090028d58c586ffc229641193612861490
kirKirKir22 <XcPfZjQi7hRGeN@>
2023-07-15 20:09:08 +0300

    version_1

---
+++ src/main/java/com/example/calculator_3/CalculatorService.java
@@ -0,0 +1,15 @@
+package com.example.calculator_3;
+
+public interface CalculatorService {
+
+    String helloCalculator();
+
+    String addTwoNumbers(int num1, int num2);
+
+    String subtractTwoNumbers(int num1, int num2);
+
+    String multiplyTwoNumbers(int num1, int num2);
+
+    String divideTwoNumbers(int num1, int num2);
+
+}
